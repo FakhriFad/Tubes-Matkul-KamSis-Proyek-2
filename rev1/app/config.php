b53fcce4a11784eb14bc4bf036e1d268d4706588
FakhriFad <fakhfad@gmail.com>
2026-04-16 10:13:12 +0700

    Remove file

--- rev1/app/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $databaseHost = 'Fakhri-ServerDB';
-    $username = 'root';
-    $password = '';
-    $databaseName = 'test';
-
-    $mysqli = mysqli_connect($databaseHost, $username, $password, $databaseName);
-?>
